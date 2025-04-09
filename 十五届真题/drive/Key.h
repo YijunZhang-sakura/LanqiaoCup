70cfbfaf65f336ca91aec96b099c01aba1556e69
YijunZhang <3073554169@qq.com>
2025-04-09 20:50:40 +0800

    Delete 十五届真题 directory

--- 十五届真题/drive/Key.h
+++
@@ -1,3 +0,0 @@
-#include <su_common_typedef.h>
-#include <stc15f2k60s2.h>
-uint8_t Key_Read();
--- 十五届真题/drive/Seg.h
+++
@@ -1,3 +0,0 @@
-#include <su_common_typedef.h>
-#include <stc15f2k60s2.h>
-void Seg_Display(uint8_t Wela,uint8_t Dula);
